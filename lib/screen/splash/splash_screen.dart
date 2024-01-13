cfea825d30611004fc56aa28140ea5eddccab6ee
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-01-13 01:23:38 +0000

    Remove splash screen animation

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -14,22 +14,11 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    )..forward();
-
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
-
     _getTimer();
   }
 
@@ -42,7 +31,6 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   @override
   void dispose() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
-    _controller.dispose();
     super.dispose();
   }
 
@@ -54,14 +42,11 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
         height: double.infinity,
         alignment: Alignment.center,
         decoration: const BoxDecoration(gradient: CustomGradient.backgroundGradient),
-        child: ScaleTransition(
-          scale: _animation,
-          child: Image.asset(
-            'assets/images/logo.png',
-            width: 200,
-            height: 190,
-            fit: BoxFit.contain,
-          ),
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 200,
+          height: 190,
+          fit: BoxFit.contain,
         ),
       ),
     );
